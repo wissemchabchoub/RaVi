c6ef1991d3bd5b2709bb0bc01396ba4f48b53620
loonyloop <loonyloop@posteo.net>
2019-06-22 11:31:14 +0200

    affichage des lieux proches

--- app/src/main/java/com/example/ravi/localisation/LocalisationActivity.java
+++ app/src/main/java/com/example/ravi/localisation/LocalisationActivity.java
@@ -9,7 +9,6 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,11 +16,12 @@ import com.example.ravi.R;
 import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
-import com.google.android.libraries.places.api.model.PlaceLikelihood;
 import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
 import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
 import com.google.android.libraries.places.api.net.PlacesClient;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
@@ -30,8 +30,8 @@ import static android.Manifest.permission.ACCESS_WIFI_STATE;
 public class LocalisationActivity extends AppCompatActivity {
 
     private PlacesClient placesClient;
+    List<Place.Field> placeFields;
     private TextView responseView;
-    private FieldSelector fieldSelector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,30 +40,33 @@ public class LocalisationActivity extends AppCompatActivity {
 
         String apiKey = getString(R.string.places_api_key);
 
+        // initialisation de l'API Places
         Places.initialize(getApplicationContext(), apiKey);
+        // géolocalisation
         placesClient = Places.createClient(this);
 
-        // Set view objects
-        List<Place.Field> placeFields =
-                FieldSelector.getPlaceFields(
-                        Place.Field.ADDRESS_COMPONENTS, Place.Field.PHONE_NUMBER, Place.Field.WEBSITE_URI, Place.Field.OPENING_HOURS);
-        fieldSelector =
-                new FieldSelector(
-                        findViewById(R.id.use_custom_fields),
-                        findViewById(R.id.custom_fields_list),
-                        placeFields);
+        // ci-dessous sont listés les champs à ne pas récupérer
+        placeFields = getPlaceFields(
+                Place.Field.ID,
+                Place.Field.LAT_LNG,
+                Place.Field.PHOTO_METADATAS,
+                Place.Field.PLUS_CODE,
+                Place.Field.PRICE_LEVEL,
+                Place.Field.USER_RATINGS_TOTAL,
+                Place.Field.VIEWPORT,
+                Place.Field.ADDRESS_COMPONENTS,
+                Place.Field.PHONE_NUMBER,
+                Place.Field.WEBSITE_URI,
+                Place.Field.OPENING_HOURS
+        );
+
         responseView = findViewById(R.id.response);
         setLoading(false);
 
-        // Set listeners for programmatic Find Current Place
         findViewById(R.id.find_current_place_button).setOnClickListener((view) -> findCurrentPlace());
     }
 
-    /**
-     * Fetches a list of {@link PlaceLikelihood} instances that represent the Places the user is
-     * most
-     * likely to be at currently.
-     */
+    // vérification des permissions
     private void findCurrentPlace() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_WIFI_STATE)
                 != PackageManager.PERMISSION_GRANTED
@@ -71,37 +74,29 @@ public class LocalisationActivity extends AppCompatActivity {
                 != PackageManager.PERMISSION_GRANTED) {
             Toast.makeText(
                     this,
-                    "Both ACCESS_WIFI_STATE & ACCESS_FINE_LOCATION permissions are required",
+                    "Pour vous proposer des lieux, nous devons avoir accès à votre géolocalisation et à internet",
                     Toast.LENGTH_SHORT)
                     .show();
         }
 
-        // Note that it is not possible to request a normal (non-dangerous) permission from
-        // ActivityCompat.requestPermissions(), which is why the checkPermission() only checks if
-        // ACCESS_FINE_LOCATION is granted. It is still possible to check whether a normal permission
-        // is granted or not using ContextCompat.checkSelfPermission().
         if (checkPermission(ACCESS_FINE_LOCATION)) {
             findCurrentPlaceWithPermissions();
         }
     }
 
-    /**
-     * Fetches a list of {@link PlaceLikelihood} instances that represent the Places the user is
-     * most
-     * likely to be at currently.
-     */
+    // récupération des lieux aux alentours
     @RequiresPermission(allOf = {ACCESS_FINE_LOCATION, ACCESS_WIFI_STATE})
     private void findCurrentPlaceWithPermissions() {
         setLoading(true);
 
         FindCurrentPlaceRequest currentPlaceRequest =
-                FindCurrentPlaceRequest.newInstance(getPlaceFields());
+                FindCurrentPlaceRequest.newInstance(placeFields);
         Task<FindCurrentPlaceResponse> currentPlaceTask =
                 placesClient.findCurrentPlace(currentPlaceRequest);
 
         currentPlaceTask.addOnSuccessListener(
                 (response) ->
-                        responseView.setText(StringUtil.stringify(response, isDisplayRawResultsChecked())));
+                        responseView.setText(StringUtil.stringify(response)));
 
         currentPlaceTask.addOnFailureListener(
                 (exception) -> {
@@ -110,18 +105,6 @@ public class LocalisationActivity extends AppCompatActivity {
                 });
 
         currentPlaceTask.addOnCompleteListener(task -> setLoading(false));
-    }
-
-    //////////////////////////
-    // Helper methods below //
-    //////////////////////////
-
-    private List<Place.Field> getPlaceFields() {
-        if (((CheckBox) findViewById(R.id.use_custom_fields)).isChecked()) {
-            return fieldSelector.getSelectedFields();
-        } else {
-            return fieldSelector.getAllFields();
-        }
     }
 
     private boolean checkPermission(String permission) {
@@ -133,52 +116,18 @@ public class LocalisationActivity extends AppCompatActivity {
         return hasPermission;
     }
 
-    private boolean isDisplayRawResultsChecked() {
-        return ((CheckBox) findViewById(R.id.display_raw_results)).isChecked();
-    }
-
+    // affichage d'une image de chargement
     private void setLoading(boolean loading) {
         findViewById(R.id.loading).setVisibility(loading ? View.VISIBLE : View.INVISIBLE);
     }
+
+    // récupération des champs attaché à un lieu
+    static List<Place.Field> getPlaceFields(Place.Field... placeFieldsToOmit) {
+
+        List<Place.Field> placeFields = new ArrayList<>(Arrays.asList(Place.Field.values()));
+        placeFields.removeAll(Arrays.asList(placeFieldsToOmit));
+
+        return placeFields;
+    }
+
 }
-
-        /*
-
-        List<Place.Field> placeFields = Arrays.asList(Place.Field.NAME);
-
-        // Use the builder to create a FindCurrentPlaceRequest.
-        FindCurrentPlaceRequest request = FindCurrentPlaceRequest.builder(placeFields).build();
-
-        // Call findCurrentPlace and handle the response (first check that the user has granted permission).
-        if (ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            Task<FindCurrentPlaceResponse> placeResponse = placesClient.findCurrentPlace(request);
-
-            placeResponse.addOnCompleteListener(task -> {
-                if (task.isSuccessful()){
-                    FindCurrentPlaceResponse response = task.getResult();
-                    for (PlaceLikelihood placeLikelihood : response.getPlaceLikelihoods()) {
-                        Log.i("LOC:", String.format("Place '%s' has likelihood: %f",
-                                placeLikelihood.getPlace().getName(),
-                                placeLikelihood.getLikelihood()));
-                    }
-                } else {
-                    Exception exception = task.getException();
-                    if (exception instanceof ApiException) {
-                        ApiException apiException = (ApiException) exception;
-                        Log.e("w", "Place not found: " + apiException.getStatusCode());
-                    }
-                }
-            });
-        } else {
-            // A local method to request required permissions;
-            // See https://developer.android.com/training/permissions/requesting
-            getLocationPermission();
-        }
-
-
-    }
-
-    private void getLocationPermission() {
-    }
-
-}*/
--- app/src/main/java/com/example/ravi/localisation/StringUtil.java
+++ app/src/main/java/com/example/ravi/localisation/StringUtil.java
@@ -1,147 +1,31 @@
 package com.example.ravi.localisation;
 
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
-import com.google.android.libraries.places.api.model.AutocompletePrediction;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.model.PlaceLikelihood;
-import com.google.android.libraries.places.api.net.FetchPlaceResponse;
-import com.google.android.libraries.places.api.net.FindAutocompletePredictionsResponse;
 import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
 
-import android.graphics.Bitmap;
-import android.text.TextUtils;
-import android.widget.TextView;
-
-import androidx.annotation.Nullable;
-
-/**
- * Utility class for converting objects to viewable strings and back.
- */
 public final class StringUtil {
 
-    private static final String FIELD_SEPARATOR = "\n\t";
-    private static final String RESULT_SEPARATOR = "\n---\n\t";
+    private static final String FIELD_SEPARATOR = "\n";
+    private static final String RESULT_SEPARATOR = "\n\n\n";
 
-    static void prepend(TextView textView, String prefix) {
-        textView.setText(prefix + "\n\n" + textView.getText());
-    }
-
-    @Nullable
-    static LatLngBounds convertToLatLngBounds(
-            @Nullable String southWest, @Nullable String northEast) {
-        LatLng soundWestLatLng = convertToLatLng(southWest);
-        LatLng northEastLatLng = convertToLatLng(northEast);
-        if (soundWestLatLng == null || northEast == null) {
-            return null;
-        }
-
-        return new LatLngBounds(soundWestLatLng, northEastLatLng);
-    }
-
-    @Nullable
-    static LatLng convertToLatLng(@Nullable String value) {
-        if (TextUtils.isEmpty(value)) {
-            return null;
-        }
-
-        String[] split = TextUtils.split(value, ",");
-        if (split.length != 2) {
-            return null;
-        }
-
-        try {
-            return new LatLng(Double.parseDouble(split[0]), Double.parseDouble(split[1]));
-        } catch (NullPointerException | NumberFormatException e) {
-            return null;
-        }
-    }
-
-    static String stringify(FindAutocompletePredictionsResponse response, boolean raw) {
+    static String stringify(FindCurrentPlaceResponse response) {
         StringBuilder builder = new StringBuilder();
 
-        builder
-                .append(response.getAutocompletePredictions().size())
-                .append(" Autocomplete Predictions Results:");
-
-        if (raw) {
-            builder.append(RESULT_SEPARATOR);
-            builder.append(TextUtils.join(RESULT_SEPARATOR, response.getAutocompletePredictions()));
-        } else {
-            for (AutocompletePrediction autocompletePrediction : response.getAutocompletePredictions()) {
+        for (PlaceLikelihood placeLikelihood : response.getPlaceLikelihoods()) {
                 builder
-                        .append(RESULT_SEPARATOR)
-                        .append(autocompletePrediction.getFullText(/* matchStyle */ null));
-            }
-        }
-
-        return builder.toString();
-    }
-
-    static String stringify(FetchPlaceResponse response, boolean raw) {
-        StringBuilder builder = new StringBuilder();
-
-        builder.append("Fetch Place Result:").append(RESULT_SEPARATOR);
-        if (raw) {
-            builder.append(response.getPlace());
-        } else {
-            builder.append(stringify(response.getPlace()));
-        }
-
-        return builder.toString();
-    }
-
-    static String stringify(FindCurrentPlaceResponse response, boolean raw) {
-        StringBuilder builder = new StringBuilder();
-
-        builder.append(response.getPlaceLikelihoods().size()).append(" Current Place Results:");
-
-        if (raw) {
-            builder.append(RESULT_SEPARATOR);
-            builder.append(TextUtils.join(RESULT_SEPARATOR, response.getPlaceLikelihoods()));
-        } else {
-            for (PlaceLikelihood placeLikelihood : response.getPlaceLikelihoods()) {
-                builder
-                        .append(RESULT_SEPARATOR)
                         .append("Likelihood: ")
                         .append(placeLikelihood.getLikelihood())
                         .append(FIELD_SEPARATOR)
                         .append("Place: ")
-                        .append(stringify(placeLikelihood.getPlace()));
+                        .append(stringify(placeLikelihood.getPlace()))
+                        .append(RESULT_SEPARATOR);
             }
-        }
 
         return builder.toString();
     }
 
     static String stringify(Place place) {
-        return place.getName() + " (" + place.getAddress() + ")";
-    }
-
-    static String stringify(Bitmap bitmap) {
-        StringBuilder builder = new StringBuilder();
-
-        builder
-                .append("Photo size (width x height)")
-                .append(RESULT_SEPARATOR)
-                .append(bitmap.getWidth())
-                .append(", ")
-                .append(bitmap.getHeight());
-
-        return builder.toString();
-    }
-
-    public static String stringifyAutocompleteWidget(Place place, boolean raw) {
-        StringBuilder builder = new StringBuilder();
-
-        builder.append("Autocomplete Widget Result:").append(RESULT_SEPARATOR);
-
-        if (raw) {
-            builder.append(place);
-        } else {
-            builder.append(stringify(place));
-        }
-
-        return builder.toString();
+        return place.getName() + " (" + place.getAddress() + ")" + place.getTypes();
     }
 }
